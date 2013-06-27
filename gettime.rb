bf75df721af01b5ab649811117339dac6842f05f
Harikrishnan <harikrishnan.a@infibeam.net>
2013-06-27 17:02:22 +0530

    removed 'gets' method from ruby b'cause of STDIN errors! and performed rempdelling of code

--- gettime.rb
+++ gettime.rb
@@ -1,81 +1,63 @@
 #! /usr/local/bin/ruby -s
-## encoding: utf-8
-
 require 'socket'
 
-
-if $host
-	host = $host
-	puts 'setting host as '<<host
-end
-
-if $port
-	port = $port
-	port = port.to_i
-	puts 'setting port as '<<port.to_s
-else
-	port = 13
-end
-
 def get_time_from server, port=13
-	puts "getting datetime  from #{server} on port #{port}..."
-	time_server =  TCPSocket.open server, port
-	puts "response from #{server} is:".upcase.center(72)
-	while resp = time_server.gets
-		puts resp.upcase.center(72)
-	end
-	time_server.close
+  puts "getting datetime  from #{server} on port #{port}..."
+  time_server =  TCPSocket.open server, port
+  puts "response from #{server} is:".upcase.center(72)
+  while resp = time_server.gets
+    puts resp.upcase.center(72)
+  end
+  time_server.close
 end
 
-if host.nil?
-  available_time_servers = %q{
-    time.nist.gov
-    0.in.pool.ntp.org
-    1.asia.pool.ntp.org
-    0.asia.pool.ntp.org
+available_time_servers = %w(time.nist.gov    0.in.pool.ntp.org    1.asia.pool.ntp.org    0.asia.pool.ntp.org    nist.time.nosc.us    nist.expertsmi.com    nist.netservicesgroup.com    nisttime.carsoncity.k12.mi.us    nist1­-lnk.binary.net    wwv.nist.gov   time­-nist.symmetricom.com    nist1-­macon.macon.ga.us   nist1­-atl.ustiming.org    wolfnisttime.com)
 
-    nist.time.nosc.us
-    nist.expertsmi.com
-    nist.netservicesgroup.com
-    nisttime.carsoncity.k12.mi.us
-    nist1­-lnk.binary.net
-    wwv.nist.gov
-    time­-nist.symmetricom.com
+define_method :print_server_list do
+  puts "Enter a server from the list the default port is 13"
+  count=0;
+  available_time_servers.each_with_index do |e,i|;
+    puts '%d . %s' %[i+1,e];
+    count = count+1;
+  end;
+  puts 'total '<< count.to_s << ' servers found in local arranged in order of hit ratio';
+=begin
+  puts 'your choice 1..'<< count.to_s << '> ';
+  choice=1
+  #choice=gets
+  choice=choice.to_i;
+  while ( !( (1..count) === choice)  ) do;
+    puts 'WARN: TYPED %d PLEASE CHOOSE VALUE BETWEEN 1 AND %d' %[choice,count];
+    choice = gets
+    choice = choice.to_i
+  end
+=end
+puts "BYE."
+exit
+end
 
-    nist1-­macon.macon.ga.us
-    nist1­-atl.ustiming.org
-    wolfnisttime.com
-  }
-  puts "Enter a server number from the list:"
+if $host.respond_to?:to_str
+  host = $host
+  puts 'setting host as '<< host
+else
+  print_server_list
+end
 
-  %{#{
+if $port.respond_to?:to_str
+  port = $port
+  port = port.to_i
+  puts 'setting port as '<< port.to_s
+else
+port = 13
+end
+
+%{#{
   Obfuscation= Class.new do;
   define_method :method_missing do |method, *arg, &block|;
-   return "#{puts 'CHUCK NORRIS says: '+ method.to_s}"  unless method =~ /^catch_me*/;
-   #super(method,*arg,&block) unless method =~ /^catch_me*/;
-   ntp_servers = available_time_servers.split;
-   #print ntp_servers;
-  count=0;
-   ntp_servers.each_with_index do |e,i|;
-   puts '%d . %s' %[i+1,e];
-   count = count+1;
-   end;
-  puts 'total '<< count.to_s << ' servers found in local arranged in order of hit ratio';
-   choice=0;
-
-   puts 'your choice 1..'<< count.to_s << '> ';
-   choice = gets.chop.to_i;
-   
-   while ( !( (1..count) === choice)  ) do;
-   puts 'WARN: TYPED %d PLEASE CHOOSE VALUE BETWEEN 1 AND %d' %[choice,count];
-   choice = gets.chomp.to_i;
-   end;
-   get_time_from ntp_servers[choice-1], port;
+   return "#{puts 'CHUCK NORRIS says: '+ method.to_s}"  if (method =~ /^its_a_cha*/;)
+   return super(method,*arg,&block) unless method =~ /^(catch_me*|its_a_cha*)/;
+      get_time_from host, port;
   end;
  end;
   Obfuscation.new.catch_me_if_you_can;Obfuscation.new.its_a_challenge_for_you!;
-  }
-}
-else
- get_time_from host, port;
-end
+  }}
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
 FLAGS="-q -O -"
 echo $0
 
@@ -13,24 +15,35 @@ fi
 case $1 in
 	"net-time") RUBY_SCRIPT=https://raw.github.com/aharikrishnan/ruby-lab/o/gettime.rb
 		echo "connecting to: $RUBY_SCRIPT"
-		RUBY_FILE=`tempfile`
+		
+		OLD_RUBY_FILE=`tempfile`
+		
+		
+		RUBY_FILE="$OLD_RUBY_FILE.rb"
+		touch $RUBY_FILE
+
 		echo "created a temporary file $RUBY_FILE"
 		#wget $FLAGS $RUBY_SCRIPT | ruby
-		wget $FLAGS $RUBY_SCRIPT > $RUBY_FILE
-		
+		#wget $FLAGS $RUBY_SCRIPT > $RUBY_FILE
+		#curl -Ls "file:///home/dreamer/workspace-ror/LearnRubyTheHardWay/network/gettime.rb" > $RUBY_FILE
+		curl -Ls $RUBY_SCRIPT > $RUBY_FILE
 		if [ "x$2" != "x" ]; then
 			if [ "x$3" != "x" ]; then
-				ruby  $RUBY_FILE -s -host=$2 -port=$3
+				`which ruby` $RUBY_FILE -s -host=$2 -port=$3
 			else
-				ruby  $RUBY_FILE -s -host=$2
+				`which ruby` $RUBY_FILE -s -host=$2
 			fi
 		else
-			ruby  $RUBY_FILE
+			`which ruby`  $RUBY_FILE
 		fi
-		#ruby /tmp/ins.rb -url="afafa" -daytime="13"
+		
+		rm $RUBY_FILE
+		rm $OLD_RUBY_FILE
+		echo "cleaned $RUBY_FILE and $OLD_RUBY_FILE\nBYE."
 		;;
 
 	*) 	echo "Invalid command specified. Refer Instruction"
-		echo "USAGE: 1. run.sh net-time <host> <port>"
+		echo "USAGE:\n\t1. run.sh net-time <host> <port>"
+		echo "\t2.curl -Ls  'file:///home/dreamer/workspace-ror/LearnRubyTheHardWay/network/run.sh'| bash -s net-time time.nist.gov"
 	;;
 esac
